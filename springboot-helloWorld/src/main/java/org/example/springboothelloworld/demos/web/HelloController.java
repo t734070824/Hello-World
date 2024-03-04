e91291dd644d9ec70c3c1da85edbcb3e2a359b9d
t734070824 <734070824@qq.com>
2024-03-04 09:10:01 +0800

    hello world

--- springboot-helloWorld/src/main/java/org/example/springboothelloworld/demos/web/HelloController.java
+++ springboot-helloWorld/src/main/java/org/example/springboothelloworld/demos/web/HelloController.java
@@ -3,12 +3,17 @@ package org.example.springboothelloworld.demos.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello Spring Boot";
+    public String hello() throws UnknownHostException {
+        InetAddress addr = InetAddress.getLocalHost();
+        return "Hello Spring Boot" + addr.getHostAddress();
     }
+
 }
 
